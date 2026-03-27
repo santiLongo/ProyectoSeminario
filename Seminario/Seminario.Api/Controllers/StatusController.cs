8ff0343a6ba9d83ea53f2763886832149f7f8cf1
santiLongo <santiagomaxilongo@gmail.com>
2026-03-26 22:57:34 -0300

    Agregon un controoler de status para ver que onda

---
+++ Seminario/Seminario.Api/Controllers/StatusController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Seminario.Datos.Contextos.AppDbContext;
+
+namespace Seminario.Api.Controllers
+{
+    [Route("api/status")]
+    [ApiController]
+    public class StatusController : ControllerBase
+    {
+        private readonly IAppDbContext _ctx;
+
+        public StatusController(IAppDbContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        [HttpGet("sayHello")]
+        [AllowAnonymous]
+        public IActionResult SayHello()
+        {
+            return Ok("Tamo activo");
+        }
+    }
+}
